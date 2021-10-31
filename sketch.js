eb997526ae82ba09d7b0d08fe2bd3a916c35f93b
2123025 <lsi371@naver.com>
2021-10-31 21:36:18 +0900

    change this.edges
    this.edges의 기준에 각각 this.size를 더하고 빼서 벽에 닿자마자 튕기는 것처럼 보이게 함

--- sketch.js
+++ sketch.js
@@ -6,15 +6,15 @@ let deceleration = 0.5;
 let limitSpeed = 100;
 
 function setup() {
-  
+
   createCanvas(720, 480);
   ball = new Ball();
 }
 
 function draw() {
-  
+
   background(51);
-  
+
   if(ball.vel.mag() >= limitSpeed) {
     ball.explode();
 
@@ -23,38 +23,38 @@ function draw() {
     ball.update();
     ball.edges();
   }
-    ball.display();  
+    ball.display();
 
 }
 
 function keyPressed() {
-  
+
   if(ball.vel.mag() > speed)
     ball.vel.mult(deceleration);
 }
 
 
 function Ball() {
-  
+
   this.pos = createVector(width/2, height/2);
   this.vel = p5.Vector.random2D().mult(speed);
   this.size = 25;
   this.ballColor = 0;
   this.isBigEnough = false;
-  
-  this.update = function() {    
+
+  this.update = function() {
       this.pos.add(this.vel);
   }
 
   this.display = function() {
     noStroke();
-    
+
     fill(255, 255 - this.ballColor, 255 - this.ballColor);
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
   }
-    
-    
-  
+
+
+
   this.explode = function() {
     if(this.isBigEnough == false){
     if(this.size < 1700){
@@ -62,7 +62,7 @@ function Ball() {
       
       if(this.pos.x < 0)
         this.pos.x = 0;
-    
+
       if(this.pos.x > width)
         this.pos.x = width;
 
@@ -75,34 +75,34 @@ function Ball() {
     else
     this.isBigEnough = true
     }
-    
+
     if(this.isBigEnough == true){
       this.pos = createVector(width/2, height/2);
       this.size -= 25;
       this.ballColor -= 3;
-      
+
       if(this.size <= 10)
         this.size = 10;
     }
 
-    
+
   }
-  
+
   this.edges = function() {
-    if (this.pos.y > height ||
-        this.pos.y < 0) {
-      
+    if (this.pos.y > height - this.size ||
+        this.pos.y < this.size) {
+
       this.vel.y *= -1;
       this.vel.mult(acceleration);
       this.ballColor += 12;
     }
-    
-    if (this.pos.x > width ||
-        this.pos.x < 0) {
-      
+
+    if (this.pos.x > width - this.size ||
+        this.pos.x < this.size) {
+
       this.vel.x *= -1;
       this.vel.mult(acceleration);
       this.ballColor += 12;
-    }  
+    }
   }
 }
